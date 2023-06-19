2ebfc395a29c5449fd6ce306f8f66d7f86378cb5
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-19 20:27:55 +0530

    web-technologies

---
+++ web-technolgies/2.js
@@ -0,0 +1,11 @@
+
+function isMeaningOfLife(num) {
+    if (num === 42) {
+      return "yes";
+    } else {
+      return "no";
+    }
+  }
+  var result = isMeaningOfLife(41);
+  
+  console.log(result);
