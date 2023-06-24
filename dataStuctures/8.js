210bcabd35edb9136121d9007d3ea96d49f4f036
junayyyyyd <mohammedjunaidkhan7@gmail.com>
2023-06-24 22:46:35 +0530

    data Stuctures

--- dataStuctures/8.js
+++ dataStuctures/8.js
@@ -1,13 +1,7 @@
-var list = ["a", "b", "b", "c", "a"];
-
-function keepDuplicates(value, index) {
-  return index !== list.indexOf(value) || index !== list.lastIndexOf(value);
-}
-function capitalize(item) {
-  return item.toUpperCase();
-}
-var duplicates = list.filter(keepDuplicates);
-var capitalizedDuplicates = duplicates.map(capitalize);
-var result = capitalizedDuplicates[1];
-
-console.log(result);
+function reverse(sentence) {
+    return sentence.split(" ").reverse().join(" ");
+  }
+  var sentence = "Hello world!";
+  var result = reverse(sentence);
+  
+  console.log(result);
